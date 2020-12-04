3df8755c8236394edd5482352b1e7ca1fe4815ce
Ismael Rivas Hidalgo <ismael.rivas@epn.edu.ec>
2020-12-03 19:30:38 -0500

    Clase 08 - Actividades en Android

--- 02-android/app/src/main/java/com/example/aplicacionmovil_software/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/aplicacionmovil_software/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.aplicacionmovil_software
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botonCicloVida = findViewById<Button>(R.id.btn_ir_ciclo_vida)
+        botonCicloVida
+                .setOnClickListener{
+                    irCicloVida()
+                }
+    }
+
+    fun irCicloVida(){
+         val intentExplicito = Intent(
+             this,
+             ACicloVida::class.java
+
+        )
+        startActivity(intentExplicito)
     }
 }
